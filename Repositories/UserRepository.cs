2078ab7a72817a99252df595aeaa3fdf3c1f50c3
Alexandre Freitas <xande_liborio@hotmail.com>
2020-11-20 18:59:01 -0300

    commit

--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using CharacterSheet.Models;
@@ -12,6 +13,11 @@ namespace CharacterSheet.Repositories
             users.Add(new User {Id = 1, Username = "player1", Password = "player1", Role = "Jogador"});
             users.Add(new User {Id = 2, Username = "player2", Password = "player2", Role = "Mestre"});
             return users.Where(x => x.Username.ToLower() == username.ToLower() && x.Password == x.Password).FirstOrDefault();
+        }
+
+        internal static string Get(object username)
+        {
+            throw new NotImplementedException();
         }
     }
 }
