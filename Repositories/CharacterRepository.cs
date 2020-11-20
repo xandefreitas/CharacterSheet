9af951970a2b877dc802be0ab1960d66ee9e0146
Alexandre Freitas <xande_liborio@hotmail.com>
2020-11-20 19:05:26 -0300

    commit

--- Repositories/CharacterRepository.cs
+++ Repositories/CharacterRepository.cs
@@ -9,10 +9,24 @@ namespace CharacterSheet.Repositories
         public static Personagem Get(string nome, string criador, string raça, string classe, string historia)
         {
             var personagens = new List<Personagem>();
-            personagens.Add(new Personagem {Id = 1, Nome = "Legolas", Criador = "Username", 
-            Raça = "Jogador", Classe = "Elf", Historia = "Descrição do Elfo"});
-            personagens.Add(new Personagem {Id = 2, Nome = "Getafix", Criador = "Username", 
-            Raça = "Mestre", Classe = "Druid", Historia = "Descrição do Druida"});
+            personagens.Add(new Personagem 
+            {
+                Id = 1, 
+                Nome = "Legolas", 
+                Criador = "Username", 
+                Raça = "Jogador", 
+                Classe = "Elf", 
+                Historia = "Descrição do Elfo"
+            });
+            personagens.Add(new Personagem 
+            {
+                Id = 2, 
+                Nome = "Getafix", 
+                Criador = "Username", 
+                Raça = "Mestre", 
+                Classe = "Druid", 
+                Historia = "Descrição do Druida"
+            });
             return personagens.Where(x => x.Nome == nome && x.Criador == criador).FirstOrDefault();
         }
     }
